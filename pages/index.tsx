7beaed5609ae1c6d7fb85c85334e0cf7b45c8087
sam <skomesarook@protonmail.com>
2021-05-18 20:26:01 +1000

    remove useless shit

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,18 +7,12 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Next App Boilerplate</title>
+        <title>Starships ;)</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Box bg="grey" h="100vh" width="100%" pt="400px">
-          <VStack spacing="4">
             <HelloWorld />
-          </VStack>
-        </Box>
       </main>
-
-      <footer></footer>
     </div>
   );
 }
