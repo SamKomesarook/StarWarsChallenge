164230f1e98403866130eeef539f9c94c83c25f9
sam <skomesarook@protonmail.com>
2021-05-18 20:19:01 +1000

    add queries

---
+++ queries/Starship.jsx
@@ -0,0 +1,14 @@
+import gql from 'graphql-tag';
+
+export default gql`
+  query Starship($id: ID) {
+      starship(id: $id){
+        pilotConnection{
+          pilots{
+            name
+          }
+        }
+      }
+    }
+  
+`
