12926fc9d5c111b233279d25902d9e01af0d7458
sam <skomesarook@protonmail.com>
2021-05-18 20:25:43 +1000

    update tests

--- components/HelloWorld/HelloWorld.test.tsx
+++ components/HelloWorld/HelloWorld.test.tsx
@@ -1,9 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import { HelloWorld } from './HelloWorld';
+import { ApolloProvider } from '@apollo/client';
+import { client } from '../../lib/apollo';
 
 describe('HelloWorld', () => {
   it('renders correctly with default value', () => {
-    render(<HelloWorld />);
-    expect(screen.queryByText('Hello World')).toBeInTheDocument();
+    const page = render(<ApolloProvider client={client}><HelloWorld /></ApolloProvider>);
+    expect(page).toMatchSnapshot();
   });
 });
