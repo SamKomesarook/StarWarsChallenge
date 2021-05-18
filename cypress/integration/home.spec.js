18bf5137a9af0d3cd92bfc8f9edbfee118b18867
sam <skomesarook@protonmail.com>
2021-05-18 20:26:42 +1000

    fix test

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -3,6 +3,6 @@ import '@testing-library/cypress/add-commands';
 describe('Home page', () => {
   it('should visit home page and display hello world', () => {
     cy.visit('/');
-    cy.findAllByText('Hello World').should('exist');
+    cy.findAllByText('Top rated starships').should('exist');
   });
 });
